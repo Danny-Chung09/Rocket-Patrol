544ed34ba22e39d48c79044dc83ec18916cec73a
dchung6969 <dchung6969@gmail.com>
2022-04-10 21:19:44 -0700

    added rocket and spaceship and collision handling

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,7 +4,13 @@ class Menu extends Phaser.Scene {
     }
     
     create() {
-      this.add.text(20, 20, "Rocket Patrol Menu");
-      this.scene.start("playScene");
+        this.add.text(20, 20, "Rocket Patrol Menu");
+        this.scene.start("playScene");
+
+        // define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
   }
